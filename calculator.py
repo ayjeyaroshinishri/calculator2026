e52c3e307324cbaa462d677204b39ae127713ab0
kiruthika-codes <mkiruthika16122005@gmail.com>
2026-01-08 21:57:27 +0530

    Initial calculator with addition operation

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+# Example usage
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = add(num1, num2)
+print("Addition =", result)
